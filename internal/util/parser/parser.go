deeed575af00b408b1493cb61ba054e01db630d9
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-20 17:42:15 +0300

    feat(utils.parser): implement parser

--- internal/util/parser/parser.go
+++ internal/util/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"regexp"
+
 	"github.com/linden-honey/linden-honey-scraper-go/pkg/domain"
 
 	"log"
@@ -8,6 +10,18 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+func substringAfter(s, substr string) string {
+	const EMPTY = ""
+	if len(s) == 0 {
+		return s
+	}
+	startIndex := strings.LastIndex(s, substr)
+	if len(substr) == 0 || startIndex == -1 {
+		return EMPTY
+	}
+	return s[startIndex+len(substr):]
+}
 
 func parseHTML(html string) *goquery.Document {
 	document, err := goquery.NewDocumentFromReader(strings.NewReader(html))
@@ -19,22 +33,59 @@ func parseHTML(html string) *goquery.Document {
 
 // ParseQuote parses html and returns a quote
 func ParseQuote(html string) domain.Quote {
-	return domain.Quote{}
+	document := parseHTML(html)
+	re := regexp.MustCompile(`\s+`)
+	phrase := re.ReplaceAllLiteralString(strings.TrimSpace(document.Text()), " ")
+	return domain.Quote{
+		Phrase: phrase,
+	}
 }
 
 // ParseVerse parses html and returns a verse
 func ParseVerse(html string) domain.Verse {
-	return domain.Verse{}
+	var quotes []domain.Quote
+	for _, text := range strings.Split(html, "<br/>") {
+		quote := ParseQuote(text)
+		if len(quote.Phrase) != 0 {
+			quotes = append(quotes, quote)
+		}
+
+	}
+	return domain.Verse{
+		Quotes: quotes,
+	}
+}
+
+func parseLyrics(html string) []domain.Verse {
+	verses := make([]domain.Verse, 0)
+	re := regexp.MustCompile(`(?:<br/>\s*){2,}`)
+	for _, verseHTML := range re.Split(html, -1) {
+		verse := ParseVerse(verseHTML)
+		verses = append(verses, verse)
+	}
+	return verses
 }
 
 // ParseSong parses html and returns a song
 func ParseSong(html string) domain.Song {
-	return domain.Song{}
+	document := parseHTML(html)
+	title := document.Find("h2").Text()
+	author := strings.TrimSpace(substringAfter(document.Find("p:has(strong:contains(Автор))").Text(), ": "))
+	album := strings.TrimSpace(substringAfter(document.Find("p:has(strong:contains(Альбом))").Text(), ": "))
+	lyricsHTML, _ := document.Find("p:last-of-type").Html()
+	verses := parseLyrics(lyricsHTML)
+	return domain.Song{
+		Title:  title,
+		Author: author,
+		Album:  album,
+		Verses: verses,
+	}
 }
 
 // ParsePreviews parses html and returns a song
-func ParsePreviews(html string) (previews []domain.Preview) {
+func ParsePreviews(html string) []domain.Preview {
 	document := parseHTML(html)
+	previews := make([]domain.Preview, 0)
 	document.Find("#abc_list a").Each(func(_ int, link *goquery.Selection) {
 		path, pathExists := link.Attr("href")
 		if pathExists {
--- internal/util/parser/parser_test.go
+++ internal/util/parser/parser_test.go
@@ -1,8 +1,69 @@
 package parser
 
 import (
+	"encoding/json"
 	"testing"
 )
+
+func TestParseSong(t *testing.T) {
+	html := `
+
+	<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+	<html>
+	<head>
+	<meta name="generator" content="Воспалённая фантазия василыча">
+	<META NAME="Robots" CONTENT="INDEX,FOLLOW">
+	<META NAME="Document-state" CONTENT="Dynamic">
+	<META NAME="Revizit-after" CONTENT="10 days">
+	<META HTTP-EQUIV="Content-type" CONTENT="text/html;charset=windows-1251">
+	<meta name="Keywords" lang="ru" content="го, г.о, г.о., гроб, гр.об., гр.об, гражданская оборона, группа гражданская оборона, группа ГО, Летов, Егор Летов, Игорь Летов, Е.Ф. Летов, Янка Дягилева, Егор и Опизденевшие, Коммунизм, Посев, панки, хой, анархия, андерграунд, русский прорыв, punk, go, gr.ob, grob, oborona, egor letov, Инструкция по выживанию, требьют, требьют гражданской обороны, требьют го, по плану, аккорды го, grob-records.go.ru, гроб-рекордз, новый альбом ГО, Звездопад">
+	<title>Гражданская Оборона - официальный сайт группы | Всё идёт по плану</title>
+	<style type="text/css" media="print">
+	sup	{
+	color:#CC0000;}
+	a:link		{color: #000000; text-decoration: underline; font-weight:bold;}
+	a:visited	{color: #000000; text-decoration: underline; font-weight:bold;}
+	a:hover		{color: #000000; font-weight:bold; text-decoration: underline;}
+	a:active	{color: #000000; text-decoration: underline; font-weight:bold;}
+	.img {
+	border:1px solid; border-color:#808080}
+	</style>
+	<style type="text/css" media="screen">
+	sup	{
+	color:#CC0000;}
+	a:link		{color: #000000; text-decoration: underline; font-weight:bold;}
+	a:visited	{color: #000000; text-decoration: underline; font-weight:bold;}
+	a:hover		{color: #000000; font-weight:bold; text-decoration: underline;}
+	a:active	{color: #000000; text-decoration: underline; font-weight:bold;}
+	.img {
+	border:1px solid; border-color:#808080}
+	</style>
+	</head>
+	<script language="JavaScript">
+	var path = window.opener.location.href;
+	</script>
+	<body>
+	<hr size="2">
+	<h2>Всё идёт по плану</h2><hr size="1">
+	<p><strong>Автор:</strong> Е.Летов</p><p><strong>Альбом:</strong> Всё идёт по плану</p><p>Границы&nbsp;ключ&nbsp;переломлен&nbsp;пополам<br>А&nbsp;наш&nbsp;батюшка&nbsp;Ленин&nbsp;совсем&nbsp;усоп<br>Он&nbsp;разложился&nbsp;на&nbsp;плесень&nbsp;и&nbsp;на&nbsp;липовый&nbsp;мёд<br>А&nbsp;перестройка&nbsp;всё&nbsp;идёт&nbsp;и&nbsp;идёт&nbsp;по&nbsp;плану<br>А&nbsp;вся&nbsp;грязь&nbsp;превратилась&nbsp;в&nbsp;голый&nbsp;лёд&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;и&nbsp;всё&nbsp;идёт&nbsp;по&nbsp;плану<br><br>А&nbsp;моя&nbsp;судьба&nbsp;захотела&nbsp;на&nbsp;покой<br>Я&nbsp;обещал&nbsp;ей&nbsp;не&nbsp;участвовать&nbsp;в&nbsp;военной&nbsp;игре<br>Но&nbsp;на&nbsp;фуражке&nbsp;на&nbsp;моей&nbsp;серп&nbsp;и&nbsp;молот&nbsp;и&nbsp;звезда<br>Как&nbsp;это&nbsp;трогательно&nbsp;—&nbsp;серп&nbsp;и&nbsp;молот&nbsp;и&nbsp;звезда<br>Лихой&nbsp;фонарь&nbsp;ожидания&nbsp;мотается<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;и&nbsp;всё&nbsp;идёт&nbsp;по&nbsp;плану<br><br>А&nbsp;моей&nbsp;женой&nbsp;накормили&nbsp;толпу<br>Мировым&nbsp;кулаком&nbsp;растоптали&nbsp;ей&nbsp;грудь<br>Всенародной&nbsp;свободой&nbsp;растерзали&nbsp;ей&nbsp;плоть<br>Так&nbsp;закопайте&nbsp;её&nbsp;во&nbsp;Христе&nbsp;-<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ведь&nbsp;всё&nbsp;идёт&nbsp;по&nbsp;плану<br><br>Один&nbsp;лишь&nbsp;дедушка&nbsp;Ленин&nbsp;хороший&nbsp;был&nbsp;вождь<br>А&nbsp;все&nbsp;другие&nbsp;остальные&nbsp;—&nbsp;такое&nbsp;дерьмо<br>А&nbsp;все&nbsp;другие&nbsp;враги&nbsp;и&nbsp;такие&nbsp;дураки<br>Над&nbsp;родною&nbsp;над&nbsp;отчизной&nbsp;бесноватый&nbsp;снег&nbsp;шёл<br>Я&nbsp;купил&nbsp;журнал&nbsp;&laquo;Корея&raquo;&nbsp;—&nbsp;там&nbsp;тоже&nbsp;хорошо<br>Там&nbsp;товарищ&nbsp;Ким&nbsp;Ир&nbsp;Сен&nbsp;там&nbsp;тоже&nbsp;что&nbsp;у&nbsp;нас<br>Я&nbsp;уверен,что&nbsp;у&nbsp;них&nbsp;тоже&nbsp;самое&nbsp;—&nbsp;<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;и&nbsp;всё&nbsp;идёт&nbsp;по&nbsp;плану<br><br>А&nbsp;при&nbsp;коммунизме&nbsp;всё&nbsp;будет&nbsp;заебись<br>Он&nbsp;наступит&nbsp;скоро&nbsp;—&nbsp;надо&nbsp;только&nbsp;подождать<br>Там&nbsp;всё&nbsp;будет&nbsp;бесплатно,там&nbsp;всё&nbsp;будет&nbsp;в&nbsp;кайф<br>Там&nbsp;наверное&nbsp;вощще&nbsp;не&nbsp;надо&nbsp;будет&nbsp;(умирать)<br>Я&nbsp;проснулся&nbsp;среди&nbsp;ночи&nbsp;и&nbsp;понял,&nbsp;что&nbsp;-<br><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ВСЁ&nbsp;ИДЁТ&nbsp;ПО&nbsp;ПЛАНУ</p><hr size="1">
+	<small><script language="JavaScript">
+	document.write("URL этой страницы: <a href=\"" + path + "\">" + path + "</a>");
+	</script><br>
+	Головной URL сайта: <a href="http://www.gr-oborona.ru">http://www.gr-oborona.ru</a><br>
+	Все вопросы, замечания и предложения направляйте по электронной почте: <a href="mailto:info@mirgorod.ru">info@mirgorod.ru</a><br>
+	<br>
+	Копирование материалов с сайта в зоне WWW допускается только со ссылкой на источник.<br>
+	Копирование материалов вне зоны WWW без разрешения запрещено. <br>
+	Официальный сайт группы «Гражданская Оборона» 2020 г.</small>
+	<hr size="2">
+	</body>
+	</html>
+	`
+	song := ParseSong(html)
+	t.Log(song)
+	bytes, _ := json.Marshal(song)
+	t.Log(string(bytes))
+}
 
 func TestParsePreviews(t *testing.T) {
 	html := `

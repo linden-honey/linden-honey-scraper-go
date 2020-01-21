7fb737d9e10d05adf0548f42ca63df4098ec171c
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-21 18:03:46 +0300

    refactor(utils.parser): use pointers

--- internal/util/parser/parser.go
+++ internal/util/parser/parser.go
@@ -12,13 +12,12 @@ import (
 )
 
 func substringAfterLast(s, substr string) string {
-	const EMPTY = ""
 	if len(s) == 0 {
 		return s
 	}
 	startIndex := strings.LastIndex(s, substr)
 	if len(substr) == 0 || startIndex == -1 {
-		return EMPTY
+		return ""
 	}
 	return s[startIndex+len(substr):]
 }
@@ -32,25 +31,25 @@ func parseHTML(html string) *goquery.Document {
 }
 
 // ParseQuote parses html and returns a quote
-func ParseQuote(html string) domain.Quote {
+func ParseQuote(html string) *domain.Quote {
 	document := parseHTML(html)
 	re := regexp.MustCompile(`\s+`)
 	phrase := re.ReplaceAllLiteralString(strings.TrimSpace(document.Text()), " ")
-	return domain.Quote{
+	return &domain.Quote{
 		Phrase: phrase,
 	}
 }
 
 // ParseVerse parses html and returns a verse
-func ParseVerse(html string) domain.Verse {
+func ParseVerse(html string) *domain.Verse {
 	var quotes []domain.Quote
 	for _, text := range strings.Split(html, "<br/>") {
 		quote := ParseQuote(text)
 		if len(quote.Phrase) != 0 {
-			quotes = append(quotes, quote)
+			quotes = append(quotes, *quote)
 		}
 	}
-	return domain.Verse{
+	return &domain.Verse{
 		Quotes: quotes,
 	}
 }
@@ -60,20 +59,20 @@ func parseLyrics(html string) []domain.Verse {
 	re := regexp.MustCompile(`(?:<br/>\s*){2,}`)
 	for _, verseHTML := range re.Split(html, -1) {
 		verse := ParseVerse(verseHTML)
-		verses = append(verses, verse)
+		verses = append(verses, *verse)
 	}
 	return verses
 }
 
 // ParseSong parses html and returns a song
-func ParseSong(html string) domain.Song {
+func ParseSong(html string) *domain.Song {
 	document := parseHTML(html)
 	title := document.Find("h2").Text()
 	author := substringAfterLast(document.Find("p:has(strong:contains(Автор))").Text(), ": ")
 	album := substringAfterLast(document.Find("p:has(strong:contains(Альбом))").Text(), ": ")
 	lyricsHTML, _ := document.Find("p:last-of-type").Html()
 	verses := parseLyrics(lyricsHTML)
-	return domain.Song{
+	return &domain.Song{
 		Title:  title,
 		Author: author,
 		Album:  album,

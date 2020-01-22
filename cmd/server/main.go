96cc320ebe6fb2ab7113b16a904be1ede38f7842
Alexander Babai <aliaksandr.babai@gmail.com>
2020-01-22 18:55:48 +0300

    refactor(cmd.server): add default retry props

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/linden-honey/linden-honey-scraper-go/pkg/service/scraper"
 )
@@ -10,6 +11,12 @@ import (
 func main() {
 	s := scraper.Create(&scraper.Properties{
 		BaseURL: "http://www.gr-oborona.ru",
+		Retry: scraper.RetryProperties{
+			Retries:    5,
+			Factor:     3,
+			MinTimeout: time.Second * 1,
+			MaxTimeout: time.Second * 6,
+		},
 	})
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		songs := s.FetchSongs()

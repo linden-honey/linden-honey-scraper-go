514ba18314e0e0104d81fe089d78469614623005
Alexander Babai <aliaksandr.babai@gmail.com>
2020-05-16 17:03:52 +0300

    refactor(controller.common): improvements

--- pkg/controller/common.go
+++ pkg/controller/common.go
@@ -8,19 +8,23 @@ import (
 	"github.com/pkg/errors"
 )
 
-func WriteJSON(data interface{}, status int, w http.ResponseWriter) {
+func WriteRawJSON(data []byte, status int, w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "application/json")
-	jsonBytes, err := json.Marshal(data)
+	w.WriteHeader(status)
+	_, err := w.Write(data)
+	if err != nil {
+		log.Println(errors.Wrap(err, "Error happened during response writing"))
+	}
+}
+
+func WriteJSON(data interface{}, status int, w http.ResponseWriter) {
+	dataBytes, err := json.Marshal(data)
 	if err != nil {
 		err := errors.Wrap(err, "Error happened during data marshalling")
 		log.Println(err)
-		WriteError(err, status, w)
+		WriteError(err, http.StatusInternalServerError, w)
 	} else {
-		w.WriteHeader(http.StatusOK)
-		_, err = w.Write(jsonBytes)
-		if err != nil {
-			log.Println(errors.Wrap(err, "Error happened during response writing"))
-		}
+		WriteRawJSON(dataBytes, status, w)
 	}
 }
 
--- pkg/controller/docs_controller.go
+++ pkg/controller/docs_controller.go
@@ -8,8 +8,6 @@ type DocsController struct {
 	Spec string
 }
 
-func (c *DocsController) GetSpec(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(([]byte)(c.Spec))
+func (c *DocsController) GetSpec(w http.ResponseWriter, _ *http.Request) {
+	WriteRawJSON([]byte(c.Spec), http.StatusOK, w)
 }

666d34bbd382e87dcfe53b6979f0e7862c8ca926
Alexander Babai <aliaksandr.babai@gmail.com>
2020-12-29 18:46:00 +0300

    refactor: fix lints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -185,7 +185,7 @@ func main() {
 
 	errs := make(chan error)
 	go func() {
-		c := make(chan os.Signal)
+		c := make(chan os.Signal, 1)
 		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 		errs <- fmt.Errorf("%s", <-c)
 	}()
